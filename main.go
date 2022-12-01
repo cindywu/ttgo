2a7af285f471cfb0d552dd4ad498d67173f1cdb3
cindy <1177031+cindywu@users.noreply.github.com>
2022-11-30 15:56:12 -1000

    packages

--- main.go
+++ main.go
@@ -2,8 +2,14 @@ package main
 
 import (
 	"fmt"
+	"math/rand"
 )
 
 func main() {
 	fmt.Println("You want to play?")
+	fmt.Println("My favorite number is", rand.Intn(10))
 }
+
+// func displayBoard(board [9]) {
+
+// }
