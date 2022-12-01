66209c1e8f966e48adee76ed1cd0e679a71ffced
cindy <1177031+cindywu@users.noreply.github.com>
2022-11-30 16:00:03 -1000

    imports

--- main.go
+++ main.go
@@ -1,13 +1,20 @@
 package main
 
+// import "fmt"
+// import "math"
+// import "math/rand"
+
+// Better yet, you can import multiple packages in a single line:
 import (
 	"fmt"
+	"math"
 	"math/rand"
 )
 
 func main() {
 	fmt.Println("You want to play?")
 	fmt.Println("My favorite number is", rand.Intn(10))
+	fmt.Printf("Now you have %g problems.\n", math.Sqrt(7))
 }
 
 // func displayBoard(board [9]) {
